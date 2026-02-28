62a2c1e02679ca87c99240d3b91316c4f640a1f4
jmcn24 <jmcneill24@qub.ac.uk>
2026-02-28 13:49:13 +0000

    Implement state-space to transfer function conversion

--- code/model/transfer_function.py
+++ code/model/transfer_function.py
@@ -1,1 +1,31 @@
+import numpy as np 
+from scipy.signal import ss2tf 
+
+# Define matrices
+A = np.array([
+    [0, 1],
+    [-2691.2, -16.08]
+])
+
+B = np.array([
+ [0],
+ [4.62]
+])
+
+C = np.array([
+    [1, 0]
+])
+
+D = np.array([[0]])
+
+# Transfer function
+num, den = ss2tf(A, B ,C, D) 
+
+print("Numerator:", num)
+print("Denominator:", den )
+
+# Poles
+poles = np.linalg.eigvals(A)
+
+print("Poles:", poles)
 
